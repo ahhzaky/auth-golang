220b7473cc71192afe5338e4680195d61575f459
Zaky Lp <siahmadzaky@gmail.com>
2023-06-19 12:30:02 +0700

    :tada: initial commit

---
+++ platform/router/router.go
@@ -0,0 +1,45 @@
+// platform/router/router.go
+
+package router
+
+import (
+	"encoding/gob"
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+
+	"exp-login/platform/authenticator"
+	"exp-login/platform/middleware"
+	"exp-login/web/app/callback"
+	"exp-login/web/app/login"
+	"exp-login/web/app/logout"
+	"exp-login/web/app/user"
+)
+
+// New registers the routes and returns the router.
+func New(auth *authenticator.Authenticator) *gin.Engine {
+	router := gin.Default()
+
+	// To store custom types in our cookies,
+	// we must first register them using gob.Register
+	gob.Register(map[string]interface{}{})
+
+	store := cookie.NewStore([]byte("secret"))
+	router.Use(sessions.Sessions("auth-session", store))
+
+	router.Static("/public", "web/static")
+	router.LoadHTMLGlob("web/template/*")
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "home.html", nil)
+	})
+	router.GET("/login", login.Handler(auth))
+	router.GET("/callback", callback.Handler(auth))
+
+	router.GET("/user", middleware.IsAuthenticated, user.Handler)
+	router.GET("/logout", logout.Handler)
+
+	return router
+}
